9ae58106aeda76996a8d264da713d2b4ff19daef
Emina Džaferović <emina.dzaferovic@bbi.ba>
2023-02-21 14:00:06 +0100

    Change in the comments

--- API/Program.cs
+++ API/Program.cs
@@ -6,14 +6,8 @@ ThesaurusRepository t = new ThesaurusRepository();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-
-// var provider = builder.Services.BuildServiceProvider();
-// var config = provider.GetRequiredService<IConfiguration>();
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(opt => {
@@ -24,14 +18,8 @@ builder.Services.AddCors(opt => {
 
 builder.Services.AddScoped<IThesaurusRepository, ThesaurusRepository>();
 
-// builder.Services.AddSingleton<IConnectionMultiplexer>(c => {
-//                     var configuration = ConfigurationOptions.Parse(config.GetConnectionString("Redis"), true);
-//                     return ConnectionMultiplexer.Connect(configuration);
-//                 });
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
